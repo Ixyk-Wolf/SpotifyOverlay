c35bc72404d31cf5c73c5b3342e2a027da2a9d8b
guillaumederybel <guillaume.derybel@protonmail.com>
2023-01-29 12:52:53 +0100

    Update window, and add vertical and horizontal position choice

--- SpotifyOverlay.py
+++ SpotifyOverlay.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, StringVar, Label
+from tkinter import Tk, StringVar, Label, NW
 from io import BytesIO
 from operator import __floordiv__
 from urllib3 import PoolManager
@@ -26,7 +26,6 @@ class SpotifyOverlay:
         self.window.attributes('-topmost', True)
         self.window.overrideredirect(True)
         self.window.configure(bg=background_color)
-        self.window.geometry('+15+15')
 
         self.title = StringVar()
         self.artist = StringVar()
@@ -46,6 +45,27 @@ class SpotifyOverlay:
         self.vertical_screen_position = vertical_screen_position
         self.horizontal_screen_position = horizontal_screen_position
 
+        self.screen_width = self.window.winfo_screenwidth()
+        self.screen_height = self.window.winfo_screenheight()
+
+        self.window_width = 219
+        self.window_height = 68
+
+        if self.horizontal_screen_position == 'right':
+            horizontal_window_position = str(self.screen_width - self.window_width - 15)
+        elif self.horizontal_screen_position == 'middle':
+            horizontal_window_position = str(int(self.screen_width/2 - self.window_width/2))
+        else:
+            horizontal_window_position = '15'
+
+        if self.vertical_screen_position == 'bottom':
+            vertical_window_position = str(self.screen_height - self.window_height - 15)
+        elif self.vertical_screen_position == 'middle':
+            vertical_window_position = str(int(self.screen_height/2 - self.window_height/2))
+        else:
+            vertical_window_position = '15'
+        
+        self.window.geometry(str(self.window_width) + 'x' + str(self.window_height) + '+' + horizontal_window_position + '+' + vertical_window_position) 
         self.create_labels()
         self.update_content()
         self.window.mainloop()
@@ -59,30 +79,32 @@ class SpotifyOverlay:
             textvariable=self.title,
             fg=self.title_font_color,
             font=(self.font_name , self.time_font_size),
-            bg=self.background_color
+            bg=self.background_color,
+            width=20,
+            anchor='w'
         )
-        label_title.grid(row=0, column=0, ipadx=3, sticky="SW")
+        label_title.grid(row=0, column=0, padx=(5,0))
 
         label_artist = Label(
             self.window,
             textvariable=self.artist,
             fg=self.artist_font_color,
             font=(self.font_name, self.artist_font_size),
-            bg=self.background_color
+            bg=self.background_color,
+            width=20,
+            anchor='nw'
         )
-        label_artist.grid(row=1, column=0, sticky="NW", ipadx=3)
+        label_artist.grid(row=1, column=0, padx=(5,0), sticky=NW)
 
         label_time = Label(
             self.window,
             textvariable=self.time,
             fg=self.time_font_color,
             font=(self.font_name, self.time_font_size),
-            bg=self.background_color
+            bg=self.background_color,
+            width=20
         )
-        label_time.grid(row=2, column=0, sticky="N", ipadx=3)
-
-        label_image = Label(self.window, image = self.image, bg=self.background_color)
-        label_image.grid(row=0, column=1, rowspan=3, ipadx=5, ipady=5)
+        label_time.grid(row=2, column=0, padx=(5,0))
 
         self.title.set("Loading...")
         self.artist.set("Loading...")
@@ -93,17 +115,25 @@ class SpotifyOverlay:
         Update content with value queried from Spotify.
         """
         current_user_playing_track = self.spotify_object.current_user_playing_track()
-        if self.last_user_playing_track == current_user_playing_track:
-            self.hide()
+        if self.title.get() == current_user_playing_track ["item"]["name"]:
+            self.show()
+            # If it's the same Music, it will only update time
+            progress = current_user_playing_track ["progress_ms"]
+            length = current_user_playing_track ["item"]["duration_ms"]
+            intotrackm, intotracks = self.parse_duration(progress)
+            lentrackm, lentracks = self.parse_duration(length)
+            self.time.set(
+                f"{intotrackm:02}:{intotracks:02} / {lentrackm:02}:{lentracks:02}")
         else:
             self.show()
             # Update image
             image_url = current_user_playing_track["item"]["album"]["images"][2]["url"]
             image_request = PoolManager().request("GET", image_url)
-            self.image = ImageTk.PhotoImage((Image.open(BytesIO(image_request.data)).resize((54, 54))))
-
-            label_image = Label(self.window, image=self.image, bg=self.background_color)
-            label_image.grid(row=0, column=1, rowspan=3, ipadx=5, ipady=5)
+            self.image = ImageTk.PhotoImage(
+                (Image.open(BytesIO(image_request.data)).resize((54, 54)))
+            )
+            label_image = Label(self.window, image=self.image, bg=self.background_color, width=54)
+            label_image.grid(row=0, column=1, rowspan=3, padx=(5,5), pady=(5,5))
 
             title = current_user_playing_track ["item"]["name"]
             artist = current_user_playing_track ['item']['artists'][0]['name']
